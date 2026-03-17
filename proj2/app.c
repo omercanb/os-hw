ce71758d8d0aef0db41a782a0cad4155cf8599d7
= <=>
2026-03-17 18:06:49 +0000

    dev

--- proj2/app.c
+++ proj2/app.c
@@ -122,12 +122,12 @@ void test_main_thread_exit() {
 
 int main(int argc, char **argv) {
     tus_init(ALG_FCFS);
-    // test_main_thread_exit();
+    test_main_thread_exit();
     // int a = tus_create_thread(worker_a, NULL);
     // tus_yield(a);
-    int d = tus_create_thread(worker_d, NULL);
-    int ret = tus_join(d);
-    assert(d == ret);
+    // int d = tus_create_thread(worker_d, NULL);
+    // int ret = tus_join(d);
+    // assert(d == ret);
     tus_exit();
     exit(0);
     int *tids;
