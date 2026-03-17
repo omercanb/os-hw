949d3a6fc909c7678b0432b13812669b21801cd8
= <=>
2026-03-17 17:55:09 +0000

    dev

--- proj2/app.c
+++ proj2/app.c
@@ -120,7 +120,7 @@ void test_main_thread_exit() {
 }
 
 int main(int argc, char **argv) {
-    tus_init(0);
+    tus_init(ALG_FCFS);
     // test_main_thread_exit();
     // int a = tus_create_thread(worker_a, NULL);
     // tus_yield(a);
